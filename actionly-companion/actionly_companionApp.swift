efca399bc54ed40eb08ed5e52fcccef8b6ebf7f0
= <kevingruber01@protonmail.com>
2026-01-16 11:39:53 +0100

    Initial Commit

---
+++ actionly-companion/actionly_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  actionly_companionApp.swift
+//  actionly-companion
+//
+//  Created by Kevin Gruber on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct actionly_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
